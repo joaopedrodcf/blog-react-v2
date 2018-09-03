2c8b323ac50c7633ba4db56352f2c0addeef06b9
joaopedrodcf <joaoferr93@gmail.com>
2018-09-03 21:36:28 +0100

    feat: Hello world

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return <div className="App">Hello world</div>;
+    }
+}
+
+export default App;
