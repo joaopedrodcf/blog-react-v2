d254f43424c35962346919771040ce48cee5aad1
rubensilva3 <rubentiago3@gmail.com>
2018-09-03 23:26:41 +0100

    feat: Add clock to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 /* eslint-disable react/prefer-stateless-function */
 
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.tick = this.tick.bind(this);
+
+        this.state = { date: new Date() };
+    }
+    componentDidMount() {
+        setInterval(this.tick, 1000);
+    }
+
+    tick() {
+        this.setState({ date: new Date() });
+    }
+
     render() {
-        return <div className="App">Hello world</div>;
+        return (
+            <div>
+                <Button color="primary">Hello world </Button>
+                <div>{this.state.date.toLocaleTimeString()}</div>
+            </div>
+        );
     }
 }
 
